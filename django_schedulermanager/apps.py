5b35c96009218c121fdf7959bbd5b41c3ff4233a
Marco Acierno <marcoacierno96@gmail.com>
2016-08-09 18:14:19 +0200

    Initial method finder

--- django_schedulermanager/apps.py
+++ django_schedulermanager/apps.py
@@ -1,7 +1,23 @@
 from __future__ import unicode_literals
 
+import inspect
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class DjangoSchedulermanagerConfig(AppConfig):
     name = 'django_schedulermanager'
+
+    def ready(self):
+        apps = settings.INSTALLED_APPS
+
+        for app in apps:
+            try:
+                jobs_module = __import__(app + '.jobs')
+            except ImportError:
+                continue
+
+            members = inspect.getmembers(jobs_module)
+            import pdb; pdb.set_trace()
+            pass
