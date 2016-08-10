09bd9d2ca763eda5f8c32e84a2b9ab98a790144e
Marco Acierno <marcoacierno96@gmail.com>
2016-08-10 16:45:13 +0200

    Add RQ_QUEUES in test project
    I use the port 6374 because I already have redis on my
    machine on the default port and I'm using docker for this
    redis instance

--- test-project/testproject/settings.py
+++ test-project/testproject/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+RQ_QUEUES = {
+    'default': {
+        'URL': '127.0.0.1:6374:0'
+    }
+}
